101a3ceebe98156318a177edd636699cfa8957d6
Hasan <hasanyousaf256@gmail.com>
2021-01-14 16:39:02 -0500

    Created new project. Added Ex1.cpp

---
+++ Week 1/Ex1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Circle
+{
+private:
+
+public:
+	double radius;
+public:
+	void serRadius(double r)
+	{
+		radius = r;
+	}
+	double calcArea()
+	{
+		return 3.14 * pow(radius, 2);
+	}
+};
+
+int main()
+{
+	Circle circle1, circle2;
+
+	circle1.serRadius(1);
+	circle2.serRadius(2.5);
+
+	cout << "The area of circle 1 is " << circle1.calcArea() << endl;
+	cout << "The area of circle 2 is " << circle2.calcArea() << endl;
+
+
+	return 0;
+}
+	
