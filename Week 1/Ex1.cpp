a8d1c546b4ce748e3c7e8c416e3c996af032415b
Hasan <hasanyousaf256@gmail.com>
2021-01-14 17:09:27 -0500

    Modified functions to be regular not inline.

--- Week 1/Ex1.cpp
+++ Week 1/Ex1.cpp
@@ -10,22 +10,16 @@ private:
 public:
 	double radius;
 public:
-	void serRadius(double r)
-	{
-		radius = r;
-	}
-	double calcArea()
-	{
-		return 3.14 * pow(radius, 2);
-	}
+	void setRadius(double r);
+	double calcArea();
 };
 
 int main()
 {
 	Circle circle1, circle2;
 
-	circle1.serRadius(1);
-	circle2.serRadius(2.5);
+	circle1.setRadius(1);
+	circle2.setRadius(2.5);
 
 	cout << "The area of circle 1 is " << circle1.calcArea() << endl;
 	cout << "The area of circle 2 is " << circle2.calcArea() << endl;
@@ -33,4 +27,12 @@ int main()
 
 	return 0;
 }
-	
+void Circle::setRadius(double r)
+{
+	radius = r;
+}
+
+double Circle::calcArea()
+{
+	return 3.14 * pow(radius, 2);
+}
