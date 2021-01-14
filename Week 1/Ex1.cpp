71914f5fe1c192144086b9d9cb7fe386c18c71b3
Hasan <hasanyousaf256@gmail.com>
2021-01-14 17:33:12 -0500

    Added Example 2 to the project

--- Week 1/Ex1.cpp
+++ Week 1/Ex1.cpp
@@ -14,19 +14,19 @@ public:
 	double calcArea();
 };
 
-int main()
-{
-	Circle circle1, circle2;
-
-	circle1.setRadius(1);
-	circle2.setRadius(2.5);
-
-	cout << "The area of circle 1 is " << circle1.calcArea() << endl;
-	cout << "The area of circle 2 is " << circle2.calcArea() << endl;
-
-
-	return 0;
-}
+//int main()
+//{
+//	Circle circle1, circle2;
+//
+//	circle1.setRadius(1);
+//	circle2.setRadius(2.5);
+//
+//	cout << "The area of circle 1 is " << circle1.calcArea() << endl;
+//	cout << "The area of circle 2 is " << circle2.calcArea() << endl;
+//
+//
+//	return 0;
+//}
 void Circle::setRadius(double r)
 {
 	radius = r;
